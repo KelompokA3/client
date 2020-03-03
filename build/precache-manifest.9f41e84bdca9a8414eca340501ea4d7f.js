7e591c5890a3abd62cfdadb81e39c837494f9520
rayhantif <rayhan.zharfan.tif18@polban.ac.id>
2020-03-03 13:37:05 +0700

    first commit

---
+++ build/precache-manifest.9f41e84bdca9a8414eca340501ea4d7f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d14b0630088247951aa14e9048573169",
+    "url": "/client/build/index.html"
+  },
+  {
+    "revision": "a615bf9aef95784565e0",
+    "url": "/client/build/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "813a6c741159d3082327",
+    "url": "/client/build/static/css/main.0420f88b.chunk.css"
+  },
+  {
+    "revision": "a615bf9aef95784565e0",
+    "url": "/client/build/static/js/2.d9d74e2e.chunk.js"
+  },
+  {
+    "revision": "813a6c741159d3082327",
+    "url": "/client/build/static/js/main.b561d7e6.chunk.js"
+  },
+  {
+    "revision": "9388a4bf6ad820b9564c",
+    "url": "/client/build/static/js/runtime~main.0d323570.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/client/build/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "f1601e82c1dc0781ad5e1749a7c8e4e1",
+    "url": "/client/build/static/media/bg2.f1601e82.jpg"
+  },
+  {
+    "revision": "cadb5e001e0a014a8bedbd9e74381f95",
+    "url": "/client/build/static/media/logo.cadb5e00.svg"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/client/build/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/client/build/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/client/build/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/client/build/static/media/tab-tv.9f91203b.png"
+  }
+]);
